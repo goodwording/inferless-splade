1ec0686971cc56df2df0f8b09d98f14afe96b513
Rishi Ishairzay <windowasher@gmail.com>
2024-10-23 12:37:19 -0400

    Add an example prompt

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,6 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["There is a fine [MASK] in the forest"]
+        'example': ["This is a test prompt"]
     }
 }
